ca76f528a291679cb55dbb333e111c5d380c8abf
Yuraima Estevez <yurm04@gmail.com>
2024-12-15 13:20:46 -0600

    test convert_date_to_utc_string

--- src/dates.rs
+++ src/dates.rs
@@ -18,32 +18,66 @@ pub fn now() -> String {
 }
 
 pub fn convert_date_to_utc_string(date_arg: String) -> Result<String, String> {
-    let parsed_date =
-        chrono::NaiveDate::parse_from_str(&date_arg, "%m-%d-%Y").expect("Failed to parse date");
+    let parsed_date = chrono::NaiveDate::parse_from_str(&date_arg, "%m-%d-%Y");
 
-    let utc_date = parsed_date.and_hms_opt(0, 0, 0);
+    let date = match parsed_date.is_err() {
+        true => return Err("Unable to parse from string".to_string()),
+        false => parsed_date.unwrap(),
+    };
 
-    if utc_date.is_none() {
-        return Err("Failed to parse date".to_string());
+    let utc_date = date.and_hms_opt(0, 0, 0);
+
+    match utc_date.is_none() {
+        true => return Err("Unable to convert to UTC".to_string()),
+        false => {
+            return Ok(
+                DateTime::<Utc>::from_naive_utc_and_offset(utc_date.expect("Sho"), Utc).to_string(),
+            )
+        }
     }
-
-    Ok(DateTime::<Utc>::from_naive_utc_and_offset(utc_date.expect("Sho"), Utc).to_string())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    const DATE_INPUT: &str = "09-09-2023";
+    const DATE_OUTPUT: &str = "2023-09-09 00:00:00 UTC";
+
     fn mock_now() -> String {
-        "10-12-2021".to_string()
+        DATE_OUTPUT.to_string()
     }
 
     #[test]
     fn parse_date_with_empty_date() {
         assert_eq!(
-            Ok("10-12-2021".to_string()),
+            Ok(DATE_OUTPUT.to_string()),
             parse_date(None, true, mock_now)
         );
         assert_eq!(Ok("".to_string()), parse_date(None, false, mock_now));
+    }
+
+    #[test]
+    fn parse_date_with_date() {
+        assert_eq!(
+            Ok(DATE_OUTPUT.to_string()),
+            parse_date(Some(DATE_INPUT.to_string()), true, mock_now),
+        )
+    }
+
+    #[test]
+    fn convert_date_to_utc_string_valid_input() {
+        assert_eq!(
+            Ok(DATE_OUTPUT.to_string()),
+            convert_date_to_utc_string(DATE_INPUT.to_string())
+        );
+    }
+
+    #[test]
+    fn convert_date_to_utc_string_invalid_input() {
+        assert_eq!(
+            Err("Unable to parse from string".to_string()),
+            convert_date_to_utc_string("foobar".to_string())
+        )
     }
 }
